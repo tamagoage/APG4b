9549e2ddd3b319544ece0d15a3c711f5da492724
tamagoage <116415023+tamagoage@users.noreply.github.com>
2023-07-29 22:43:15 +0900

    14~18途中

--- AOJ/C++/ex14.cpp
+++ AOJ/C++/ex14.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
-    
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int min_number = min({A, B, C});
+    int max_number = max({A, B, C});
+    cout << max_number - min_number << endl;
 }
--- AOJ/C++/ex18.cpp
+++ AOJ/C++/ex18.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++) {
+        cin >> A.at(i) >> B.at(i);
+    }
+
+    // ここにプログラムを追記
+    // (ここで"試合結果の表"の2次元配列を宣言)
+    vector<vector<char>> game_result(N, vector<char>(N , '-'));
+
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < M; j++){
+            if (game_result.at(i) == A.at(i) && game_result.at(j) == B.at(j)){
+                game_result.at(i).at(j) = 'o';
+                game_result.at(j).at(i) = 'x';
+            }
+        }
+    }
+
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < M; j++){
+            cout << game_result.at(i).at(j) << " ";
+        }
+        
+    }
     
 }
