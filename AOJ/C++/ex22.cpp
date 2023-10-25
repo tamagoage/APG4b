b3585c517b90ff84a41a4015bb74ea21637e9773
tamagoage <116415023+tamagoage@users.noreply.github.com>
2023-10-25 17:22:54 +0900

    ローカルのみビルドエラー

--- AOJ/C++/ex22.cpp
+++ AOJ/C++/ex22.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    // N個のpairが与えられる
+    int N;
+    cin >> N;
+    vector<pair<int, int>> pair_array(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        pair_array[i] = make_pair(b, a);
+    }
+
+    sort(pair_array.begin(), pair_array.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << pair_array.at(i).second << " " << pair_array.at(i).first << endl;
+    }
 }
