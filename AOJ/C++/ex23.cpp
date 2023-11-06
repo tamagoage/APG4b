02ff9b474fb893c37ad6e445ee8505b1b5150296
tamagoage <116415023+tamagoage@users.noreply.github.com>
2023-11-06 21:41:05 +0900

    ex23

--- AOJ/C++/ex23.cpp
+++ AOJ/C++/ex23.cpp
@@ -1,6 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> NumArray(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> NumArray.at(i);
+    }
+
+    int maxNum = *max_element(NumArray.begin(), NumArray.end());
+    int minNum = *min_element(NumArray.begin(), NumArray.end());
+
+    // priority_queue<int> NumArrayPQ;
+    // for (int i = 0; i < N; i++)
+    // {
+    //     NumArrayPQ.push(NumArray.at(i));
+    // }
+
+    // for (int i = 0; i < minNum; i++) {
+    //     if
+    // }
+
+    map<int, int> NumMap;
+    for (int i = 0; i < N; i++)
+    {
+        if (NumMap.count(NumArray.at(i)))
+        {
+            NumMap.at(NumArray.at(i))++;
+        }
+        else
+        {
+            NumMap[NumArray.at(i)] = 1;
+        }
+    }
+
+    // 以降答え参考
+    int maxNumCount = 0;
+    int ans = 0;
+
+    for (int x : NumArray)
+    {
+        if (NumMap.at(x) > maxNumCount)
+        {
+            maxNumCount = NumMap.at(x);
+            ans = x;
+        }
+    }
+
+    cout << ans << " " << maxNumCount << endl;
 }
